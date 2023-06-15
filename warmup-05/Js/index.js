29c82f955452defac3eb1c5131b143acddb5d15e
Maximilian741 <maximilian.casteel@umconnect.umt.edu>
2023-06-14 23:48:07 -0600

    I am not sure if you wanted me to code in javascruipt functionality yet.  It seemed a bit Far off from what we have learned so far and what was in the workbook. I could try and link it to the site that was used in the examples for the forms and submit buttons.  I will do this tomorrow.

---
+++ warmup-05/Js/index.js
@@ -0,0 +1,3 @@
+//I am not sure if you wanted me to code in javascruipt functionality yet.  It seemed a bit 
+//Far off from what we have learned so far and what was in the workbook.
+//I could try and link it to the site that was used in the examples for the forms and submit buttons.  I will do this tomorrow.
